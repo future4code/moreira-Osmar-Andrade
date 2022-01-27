556338afbe51f2e84a1bbf9cac07b8d58249a40a
osmar andrade <osmarmacielandrade@gmail.com>
2022-01-26 21:49:13 -0300

    projeto-labenusers

---
+++ modulo2/apis-labenusers/osmar-labenusers/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import TelaCadastro from "./components/TelaCadastro"
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+         
+         <TelaCadastro/>
+      </div>
+    )
+  }
+}
+ 
+export default App;
