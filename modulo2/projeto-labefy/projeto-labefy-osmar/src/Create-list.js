ec5a2493d54f0805bdd29e58f22c6cd7ed309ddd
osmar andrade <osmarmacielandrade@gmail.com>
2022-01-28 00:02:27 -0300

    projeto-labefy-andrade

---
+++ modulo2/projeto-labefy/projeto-labefy-osmar/src/Create-list.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class CreateList extends React.Component{
+    render(){
+        return(
+          <div>creatList</div>
+        )
+    }
+
+}
+
+export default CreateList;
---
+++ modulo2/projeto-labefy/projeto-labefy-osmar/src/PlayList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class PlayList extends React.Component{
+    render (){
+        return(
+            <div>playlist</div>
+        )
+    }
+}
+
+export default PlayList;
