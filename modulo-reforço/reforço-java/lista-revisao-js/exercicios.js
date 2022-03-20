5971d8267e49460431e1f5d9e2b28d585f383699
osmar andrade <osmarmacielandrade@gmail.com>
2022-03-20 17:03:05 -0300

    execicio 7

--- modulo-reforço/reforço-java/lista-revisao-js/exercicios.js
+++ modulo-reforço/reforço-java/lista-revisao-js/exercicios.js
@@ -54,7 +54,12 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let resto = maiorNumero % a
+    if (resto === 0){
+       return true
+   } else{
+       return false
+   }
 }
 
 // EXERCÍCIO 09
