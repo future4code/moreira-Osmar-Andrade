fe267530acef0f64f535283b548c116ad4fabde1
osmar andrade <osmarmacielandrade@gmail.com>
2022-03-22 18:08:39 -0300

    exercicio-back-end

---
+++ modulo-4/src/index.js
@@ -0,0 +1,12 @@
+// // no terminal eu coloco: node-nome-do-arquivo  os valores que quero passar 
+
+// Crie um programa que receba seu nome e sua idade. Após receber estes valores, 
+// imprima no console uma mensagem que siga a seguinte estrutura:
+// Olá, (Nome)! Você tem (sua idade) anos."
+
+
+const idade = Number(process.argv[2])
+const nome = Number(process.argv[3])
+
+
+
